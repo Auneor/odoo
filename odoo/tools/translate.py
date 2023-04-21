475f7d30abafe49a29e7ca589364606e775b1738
Angelo-Auneor <angelo@auneor-conseil.fr>
2023-04-21 10:35:55 +0200

    R-2602 t19833: don't group translations

--- odoo/tools/translate.py
+++ odoo/tools/translate.py
@@ -740,19 +740,22 @@ class PoFileWriter:
         # we now group the translations by source. That means one translation per source.
         grouped_rows = {}
         for module, type, name, res_id, src, trad, comments in rows:
-            row = grouped_rows.setdefault(src, {})
-            row.setdefault('modules', set()).add(module)
-            row['translation'] = trad
-            row.setdefault('tnrs', []).append((type, name, res_id))
-            row.setdefault('comments', set()).update(comments)
+            row = grouped_rows.setdefault(src, [])
+            new_row = {}
+            new_row.setdefault('modules', set()).add(module)
+            new_row['translation'] = trad
+            new_row.setdefault('tnrs', []).append((type, name, res_id))
+            new_row.setdefault('comments', set()).update(comments)
+            row.append(new_row)
 
-        for src, row in sorted(grouped_rows.items()):
-            if not self.lang:
-                # translation template, so no translation value
-                row['translation'] = ''
-            elif not row.get('translation'):
-                row['translation'] = ''
-            self.add_entry(sorted(row['modules']), row['tnrs'], src, row['translation'], row['comments'])
+        for src, rows in sorted(grouped_rows.items()):
+            for row in rows:
+                if not self.lang:
+                    # translation template, so no translation value
+                    row['translation'] = ''
+                elif not row.get('translation'):
+                    row['translation'] = ''
+                self.add_entry(sorted(row['modules']), row['tnrs'], src, row['translation'], row['comments'])
 
         # buffer expects bytes
         self.buffer.write(str(self.po).encode())
