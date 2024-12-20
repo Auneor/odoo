037b76da5e0468ec768beb72e53b90b59bae2ee3
Angelo-Auneor <angelo@auneor-conseil.fr>
2024-12-20 15:32:34 +0100

    Remove incompatible modules popup

--- odoo/addons/base/models/ir_module.py
+++ odoo/addons/base/models/ir_module.py
@@ -421,7 +421,7 @@ class Module(models.Model):
         for module in install_mods:
             for exclusion in module.exclusion_ids:
                 if exclusion.name in install_names:
-                    raise UserError(_('Modules %r and %r are incompatible.', module.shortdesc, exclusion.exclusion_id.shortdesc))
+                    return module.button_uninstall_wizard()
 
         # check category exclusions
         def closure(module):
