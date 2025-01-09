866e8bad84ac68a928cf1aab8584babc909763d9
Angelo-Auneor <angelo@auneor-conseil.fr>
2025-01-09 12:18:58 +0100

    Revert "Remove incompatible modules popup"
    This reverts commit 282d5cc269c44f11c4e426a824ca4a7a58acbc32.

--- odoo/addons/base/models/ir_module.py
+++ odoo/addons/base/models/ir_module.py
@@ -428,12 +428,11 @@ class Module(models.Model):
         for module in install_mods:
             for exclusion in module.exclusion_ids:
                 if exclusion.name in install_names:
-                    return module.button_uninstall_wizard()
-                    # raise UserError(_(
-                    #     'Modules "%(module)s" and "%(incompatible_module)s" are incompatible.',
-                    #     module=module.shortdesc,
-                    #     incompatible_module=exclusion.exclusion_id.shortdesc,
-                    # ))
+                    raise UserError(_(
+                        'Modules "%(module)s" and "%(incompatible_module)s" are incompatible.',
+                        module=module.shortdesc,
+                        incompatible_module=exclusion.exclusion_id.shortdesc,
+                    ))
 
         # check category exclusions
         def closure(module):
